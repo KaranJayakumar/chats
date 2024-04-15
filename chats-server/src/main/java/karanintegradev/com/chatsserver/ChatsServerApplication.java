6a5e9557fe42fb2dc4afebfe60cbc4eb2537bac4
KaranJayakumar <karanjaykumar@gmail.com>
2024-04-15 23:50:20 +1000

    Initial backend commit

---
+++ chats-server/src/main/java/karanintegradev/com/chatsserver/ChatsServerApplication.java
@@ -0,0 +1,13 @@
+package karanintegradev.com.chatsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsServerApplication.class, args);
+	}
+
+}
---
+++ chats-server/src/test/java/karanintegradev/com/chatsserver/ChatsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package karanintegradev.com.chatsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
